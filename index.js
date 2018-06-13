9df209e73ee0c712b8045a9ab994494bf3e2c791
David Borah <dborah87@gmail.com>
2018-06-13 16:43:10 -0400

    Added the Array for the dates to match the reminder task subjects,  added the loop to have each reminder show up on a new line

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const fs = require('fs');//this tells node that we need this module (set of fnctions and attributes)
 console.log("Welcome to remindme");
-const reminders = "take a nap";
+
 const reminderFile = "reminders.txt";
+
+
+
+const reminders = [
+    "get some milk", //0
+    "work out", //1
+    "get groceries", //2
+    "groom the dog", //3
+    "take a nap" //4
+];
+//Adding an array for dates to match the reminder subject/body
+const dates = [  
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date()
+];
+
+//for loop 
+for(let i = 0; i < reminders.length; i += 1){
+    const reminder = reminders[i];
+    const date = dates[i];
+    const line = `${reminder}|${date}\n`;
+    fs.appendFileSync(reminderFile, line);
+}
 
 fs.writeFileSync(reminderFile, reminders);
 console.log("Wrote all of the reminders to the file");
