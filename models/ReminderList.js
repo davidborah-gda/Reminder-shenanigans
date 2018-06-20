1e35d6aa30f2202d24490221227a728f095e0dc5
David Borah <dborah87@Davids-Air.itpro.tv>
2018-06-20 18:54:08 -0400

    toFile string and to String methods changed to use the array method chaining them to an inline fucntion

--- models/ReminderList.js
+++ models/ReminderList.js
@@ -3,32 +3,33 @@ const fs = require('fs');
 const Reminder = require('./Reminder');
 
 class ReminderList {
-    constructor(filepath) {
+    constructor(filepath){
         this.filepath = filepath;
-        const lines = fs.readFileSync(this.filepath, 'utf8').split('\n');
-        const parsedLines = lines.map(line => line.split("|"));
-        const humanFriendlyLines = parsedLines.map(parsedLine => {
-            const reminder = new Reminder(parsedLine[0], parsedLine[1]);
-            return reminder;
-        });
-        this.reminders = humanFriendlyLines;
+        this.reminders = fs.readFileSync(this.filepath, 'utf8')
+                            .split('\n')
+                            .map((line) => line.split('|'))
+                            .map((parsedLine) => new Reminder(parsedLine[0], parsedLine[1]));
     }
 
-    add(reminder) {
+    add(reminder){
         this.reminders.push(reminder);
     }
 
-    toFileOutput() {
-        const fileString = this.reminders.map((reminder) => reminder.toFileString());
-        const output = fileString.join('\n');
-        return output;
+    toFileOutput(){
+        const output = this.reminders
+                            .map((reminder) => reminder.toFileString())
+                            .join('\n');
+            return output;
+    }
+   
+    toConsole(){
+        const output = this.reminders
+                        .map((reminder) => reminder.toString())
+                        .join('\n');
+                console.log(output);
     }
 
-    toConsole() {
-        const reminderStrings = this.reminders.map((reminder) => reminder.toString());
-        const output = reminderStrings.join('\n');
-        console.log(output);
-    }
+    
 }
 
 module.exports = ReminderList;
