d2bc2a0c24f1e7568547121fe8d868266a242607
David Borah <dborah87@Davids-Air.itpro.tv>
2018-06-20 20:35:29 -0400

    testing our app

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const os = require('os');
 module.exports.reminderFile = 'reminders.txt';
 module.exports.dateFormat = 'M/D/YYYY hh:mm a';
 module.exports.padding = 60;
